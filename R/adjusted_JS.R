97247f16b91e5e59b64b3ede4e14399dd6c2932f
yuanxiesa <40753490+yuanxiesa@users.noreply.github.com>
2022-11-04 20:35:10 -0500

    upload scripts for figure 8 and figure 9

--- R/adjusted_JS.R
+++ R/adjusted_JS.R
@@ -1,3 +1,23 @@
+#
+# adjusted_JS.R
+#
+# This file generate Figure 8 and Figure 9 of manuscript "Exploring Evidence Selection with the Inclusion Network"
+# by Yuanxi Fu, Caitlin Vitosky Clarke, Mark Van Moer, and Jodi Schneider
+
+# author: Yuanxi Fu
+#
+# Where to Find the data
+#
+# ExRx: Clarke, Caitlin; Lischwe Mueller, Natalie; Joshi, Manasi Ballal; Fu, Yuanxi; Schneider, Jodi (2022): 
+# The Inclusion Network of 27 Review Articles Published between 2013-2018 
+# Investigating the Relationship Between Physical Activity and Depressive Symptoms. 
+# University of Illinois at Urbana-Champaign. 
+# https://doi.org/10.13012/B2IDB-4614455_V1
+#
+# Salt: Fu, Yuanxi; Hsiao, Tzu-Kun; Joshi, Manasi Ballal (2022): 
+# The Salt Controversy Systematic Review Reports and Primary Study Reports Network Dataset . 
+# University of Illinois at Urbana-Champaign. 
+# https://doi.org/10.13012/B2IDB-6128763_V2
 
 rm(list = ls())
 
@@ -5,53 +25,130 @@ source("functions.R")
 
 # construct the two inclusion network
 # set input files: ExRx
-raw_edge_list_file <- "data/ExRx/inclusion_net_edges.csv"
-raw_attr_list_file <- "data/ExRx/article_list.csv"
-raw_search_date_file <- "data/ExRx/review_article_details.csv"
+raw_edge_list_file_exrx <- "data/ExRx/inclusion_net_edges.csv"
+raw_attr_list_file_exrx <- "data/ExRx/article_list.csv"
+raw_search_date_file_exrx <- "data/ExRx/review_article_details.csv"
 
-G_exrx <- make_exrx_graph(raw_edge_list_file, 
-                          raw_attr_list_file, 
-                          raw_search_date_file)
+G_exrx <- make_exrx_graph(raw_edge_list_file_exrx, 
+                          raw_attr_list_file_exrx, 
+                          raw_search_date_file_exrx)
 
 # set input files: Salt Controversy
-raw_edge_list_file <- "data/salt/inclusion_net_edges.csv"
-raw_attr_list_file <- "data/salt/report_list.csv"
-raw_search_date_file <- "data/salt/systematic_review_inclusion_criteria.csv"
+raw_edge_list_file_salt <- "data/salt/inclusion_net_edges.csv"
+raw_attr_list_file_salt <- "data/salt/report_list.csv"
+raw_search_date_file_salt <- "data/salt/systematic_review_inclusion_criteria.csv"
 
-G_salt <- create_salt_graph(raw_edge_list_file, 
-                            raw_attr_list_file, 
-                            raw_search_date_file)
+G_salt <- create_salt_graph(raw_edge_list_file_salt, 
+                            raw_attr_list_file_salt, 
+                            raw_search_date_file_salt)
 
-#########################################
-## Create Average Adjusted JS for ExRx ##
-#########################################
+#####################################################
+## Create Average Adjusted JS time series for ExRx ##
+#####################################################
 
 # create a dataframe to store output --
 year_list <- sort(unique(V(G_exrx)[V(G_exrx)$type == "Systematic Review Report"]$year))
 srr_list <- V(G_exrx)[V(G_exrx)$type == "Systematic Review Report"]$name
 
 # a data structure to store data
+srr_1_vector <- c()
+srr_2_vector <- c()
+adjusted_js_vector <- c()
+
+for (srr_1_name in srr_list){
+  
+  print(srr_1_name)
+  
+  for (srr_2_name in srr_list) {
+    
+    if (srr_1_name != srr_2_name){
+
+      srr_1_vector <- c(srr_1_vector, srr_1_name)
+      srr_2_vector <- c(srr_2_vector, srr_2_name)
+      
+      adjusted_js_vector <- c(adjusted_js_vector, 
+                              adjusted_js(srr_1_name, srr_2_name, G_exrx))
+
+      
+    }
+  }
+}
+
+adjusted_js_df <- data.frame("srr_1_name" = srr_1_vector, 
+                             "srr_2_name" = srr_2_vector, 
+                             "adjusted_js" = adjusted_js_vector)
+
+# clean off redundant rows: (v1, v2) and (v2, v1)
+# use a simple math since all nodes are also numbers
+# may not work with text node names
+rows_to_keep <- which(as.integer(adjusted_js_df$srr_1_name) 
+                      < as.integer(adjusted_js_df$srr_2_name))
+
+adjusted_js_df <- adjusted_js_df[rows_to_keep,]
+
+search_date_df <- read.csv(raw_search_date_file_exrx)
+
+# append search year
+# first convert datatype to character for merge
+search_date_df$article_id <- as.character(search_date_df$article_id)
+search_date_df <- select(search_date_df, "article_id", "search_year")
+
+# merge data frames to add year
+adjusted_js_df_with_year <- inner_join(x=adjusted_js_df, 
+                                       y=search_date_df, 
+                                       by=c('srr_1_name' = 'article_id'))
+
+colnames(adjusted_js_df_with_year) <- c("srr_1_name", "srr_2_name", "adjusted_js", "srr_1_year")
+
+adjusted_js_df_with_year <- inner_join(x=adjusted_js_df_with_year, 
+                                       y=search_date_df, 
+                                       by=c('srr_2_name' = 'article_id'))
+
+colnames(adjusted_js_df_with_year) <- c("srr_1_name", "srr_2_name", "adjusted_js", 
+                               "srr_1_year", "srr_2_year")
+
+# prepare vectors
 year_vector <- c()
 srr_name_vector <- c()
 avg_adjusted_js_vector <- c()
 
-for (year in year_list){
-  for (srr_name in srr_list){
+# looping through srr and year and computer average
+for (srr in srr_list){
+  
+  for (year in year_list){
     
-    function_output <- avg_adjusted_js(year=year,
-                                       srr_name = srr_name,
-                                       G = G_exrx)
+    temp_df_1 <- adjusted_js_df_with_year %>% filter(srr_1_name == srr)
+    temp_df_2 <- adjusted_js_df_with_year %>% filter(srr_2_name == srr)
     
-    year_vector <- c(year_vector, function_output$year)
+    #swap srr_1 and srr_2 if srr_2 == srr
     
-    srr_name_vector <- c(srr_name_vector, function_output$srr_name)
+    if (nrow(temp_df_2)){
+      
+      for (i in 1:nrow(temp_df_2)){
+        
+        temp_srr <- temp_df_2$srr_2_name[i]
+        temp_df_2$srr_2_name[i] <- temp_df_2$srr_1_name[i]
+        temp_df_2$srr_1_name[i] <- temp_srr
+        
+        temp_srr_year <- temp_df_2$srr_2_year[i]
+        temp_df_2$srr_2_year[i] <- temp_df_2$srr_1_year[i]
+        temp_df_2$srr_1_year[i] <- temp_srr_year
+      }
+    }
     
-    avg_adjusted_js_vector <- c(avg_adjusted_js_vector, 
-                                function_output$avg_adjusted_js)
+    # use temp_df to computer average adjusted js for 
+    temp_df <- rbind(temp_df_1, temp_df_2)
+    
+    temp_df <- filter(temp_df, srr_2_year <= year)
+    
+    avg_adjusted_js_vector <- c(avg_adjusted_js_vector, mean(temp_df$adjusted_js))
+    year_vector <- c(year_vector, year)
+    srr_name_vector <- c(srr_name_vector, srr)
   }
+    
 }
 
-
+# data structure of the resulted dataframe
 avg_adjusted_js_df <- data_frame(year = year_vector,
                                  srr_name = srr_name_vector,
                                  avg_adjusted_js = avg_adjusted_js_vector)
@@ -73,27 +170,6 @@ for (i in 1:nrow(avg_adjusted_js_df)){
   }
 }
 
-# plot
-# # one row
-# ggplot(data = avg_adjusted_js_df,
-#        mapping = aes(x = year, y = avg_adjusted_js, group = label, color = color)) +
-#   geom_line(color = "black", show.legend = FALSE) +
-#   geom_point(show.legend = FALSE) +
-#   facet_wrap(vars(label), ncol = 27) + 
-#   scale_x_continuous(limits = c(2010, 2020), breaks = c(2012, 2018), labels = c(12, 18)) +
-#   scale_y_continuous(limits = c(0, 0.20)) +
-#   labs(y = "Average Adjusted Jaccard Similarity", x = 'Year')
-#   
-# # two rows
-# ggplot(data = avg_adjusted_js_df,
-#        mapping = aes(x = year, y = avg_adjusted_js, group = label, color = color)) +
-#   geom_line(color = "black", show.legend = FALSE) +
-#   geom_point(show.legend = FALSE) +
-#   facet_wrap(vars(label), ncol = 14, nrow = 2) + 
-#   scale_x_continuous(limits = c(2010, 2020), breaks = c(2012, 2018), labels = c(2012, 2018)) +
-#   scale_y_continuous(limits = c(0, 0.20)) +
-#   labs(y = "Average Adjusted Jaccard Similarity", x = 'Year')
-
 # change color
 my_colors = c("black", "red")
 ggplot(data = avg_adjusted_js_df,
@@ -110,42 +186,113 @@ ggplot(data = avg_adjusted_js_df,
 ## Create Average Adjusted JS for Salt Controversy ##
 #####################################################
 
-# create a dataframe to store output --
 year_list <- sort(unique(V(G_salt)[V(G_salt)$type == "Systematic Review Report"]$year))
 srr_list <- V(G_salt)[V(G_salt)$type == "Systematic Review Report"]$name
 
 # a data structure to store data
+srr_1_vector <- c()
+srr_2_vector <- c()
+adjusted_js_vector <- c()
+
+for (srr_1_name in srr_list){
+  
+  for (srr_2_name in srr_list) {
+    
+    if (srr_1_name != srr_2_name){
+
+      srr_1_vector <- c(srr_1_vector, srr_1_name)
+      srr_2_vector <- c(srr_2_vector, srr_2_name)
+      
+      adjusted_js_vector <- c(adjusted_js_vector, 
+                              adjusted_js(srr_1_name, srr_2_name, G_salt))
+    }
+  }
+}
+
+adjusted_js_df <- data.frame("srr_1_name" = srr_1_vector, 
+                             "srr_2_name" = srr_2_vector, 
+                             "adjusted_js" = adjusted_js_vector)
+
+# clean off redundant rows: (v1, v2) and (v2, v1)
+# use a simple math since all nodes are also numbers
+# may not work with text node names
+rows_to_keep <- which(as.integer(adjusted_js_df$srr_1_name) 
+                      < as.integer(adjusted_js_df$srr_2_name))
+
+adjusted_js_df <- adjusted_js_df[rows_to_keep,]
+
+search_date_df <- read.csv(raw_search_date_file_salt)
+
+# append search year
+# first convert datatype to character for merge
+search_date_df$ID <- as.character(search_date_df$ID)
+search_date_df <- select(search_date_df, "ID", "last_search_year")
+
+# merge data frames to add year
+adjusted_js_df_with_year <- inner_join(x=adjusted_js_df, 
+                                       y=search_date_df, 
+                                       by=c('srr_1_name' = 'ID'))
+
+colnames(adjusted_js_df_with_year) <- c("srr_1_name", "srr_2_name", "adjusted_js", "srr_1_year")
+
+adjusted_js_df_with_year <- inner_join(x=adjusted_js_df_with_year, 
+                                       y=search_date_df, 
+                                       by=c('srr_2_name' = 'ID'))
+
+colnames(adjusted_js_df_with_year) <- c("srr_1_name", "srr_2_name", "adjusted_js", 
+                                        "srr_1_year", "srr_2_year")
+
+# prepare vectors
 year_vector <- c()
 srr_name_vector <- c()
 avg_adjusted_js_vector <- c()
 
-# loop over year and srr to fill out the vectors
-for (year in year_list){
-  for (srr_name in srr_list){
+# looping through srr and year and computer average
+for (srr in srr_list){
+  
+  for (year in year_list){
     
-    function_output <- avg_adjusted_js(year=year,
-                                       srr_name = srr_name,
-                                       G = G_salt)
+    temp_df_1 <- adjusted_js_df_with_year %>% filter(srr_1_name == srr)
+    temp_df_2 <- adjusted_js_df_with_year %>% filter(srr_2_name == srr)
     
-    year_vector <- c(year_vector, function_output$year)
+    #swap srr_1 and srr_2 if srr_2 == srr
     
-    srr_name_vector <- c(srr_name_vector, function_output$srr_name)
+    if (nrow(temp_df_2)){
+      
+      for (i in 1:nrow(temp_df_2)){
+        
+        temp_srr <- temp_df_2$srr_2_name[i]
+        temp_df_2$srr_2_name[i] <- temp_df_2$srr_1_name[i]
+        temp_df_2$srr_1_name[i] <- temp_srr
+        
+        temp_srr_year <- temp_df_2$srr_2_year[i]
+        temp_df_2$srr_2_year[i] <- temp_df_2$srr_1_year[i]
+        temp_df_2$srr_1_year[i] <- temp_srr_year
+      }
+    }
     
-    avg_adjusted_js_vector <- c(avg_adjusted_js_vector, 
-                                function_output$avg_adjusted_js)
+    # use temp_df to computer average adjusted js for 
+    temp_df <- rbind(temp_df_1, temp_df_2)
+    
+    temp_df <- filter(temp_df, srr_2_year <= year)
+    
+    avg_adjusted_js_vector <- c(avg_adjusted_js_vector, mean(temp_df$adjusted_js))
+    year_vector <- c(year_vector, year)
+    srr_name_vector <- c(srr_name_vector, srr)
   }
+  
 }
 
-# create a data frame
+# data structure of the resulted dataframe
 avg_adjusted_js_df <- data_frame(year = year_vector,
                                  srr_name = srr_name_vector,
                                  avg_adjusted_js = avg_adjusted_js_vector)
+
 
 avg_adjusted_js_df$label <- stringr::str_c("#", avg_adjusted_js_df$srr_name)
 avg_adjusted_js_df$label <- factor(avg_adjusted_js_df$label,
                                    levels = stringr::str_c("#", srr_list))
 
-# zero: the year is not the search year for the target SRR
 avg_adjusted_js_df$is_search_year <- rep(0, nrow(avg_adjusted_js_df))
 
 # add color column
@@ -153,16 +300,15 @@ for (i in 1:nrow(avg_adjusted_js_df)){
   
   if (avg_adjusted_js_df$year[i] == 
       V(G_salt)[which(V(G_salt)$name == avg_adjusted_js_df$srr_name[i])]$year){
-    # 1: the year is the search year for the target SRR
+    
     avg_adjusted_js_df$is_search_year[i] = 1
-  
-    }
+    
+  }
 }
-
-# avg_adjusted_js_df$is_search_year <- factor(avg_adjusted_js_df$is_search_year)
 
 # plot
 my_colors <- c("black", "red" )
+
 ggplot(data = avg_adjusted_js_df, 
        mapping = aes(x = year, y = avg_adjusted_js, group = label, color = factor(is_search_year))) +
   geom_point(show.legend = FALSE) +
@@ -172,3 +318,5 @@ ggplot(data = avg_adjusted_js_df,
   scale_y_continuous(limits = c(0, 1.0)) +
   scale_color_manual(values = my_colors) +
   labs(y = "Average Adjusted Jaccard Similarity", x = 'Year')
+
+
--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,7 @@
+# functions.R
+# author: Yuanxi Fu
+# this file holds necessary functions for the set of R script 
+# in the current repository
 
 library(igraph)
 library(tidyverse)
@@ -17,15 +21,15 @@ make_exrx_graph <- function(raw_edge_list_file,
   raw_search_date <- read.csv(raw_search_date_file)
   
   # filter the raw data
-  edge_list <- data_frame('from' = raw_edge_list$citing_id,
+  edge_list <- data.frame('from' = raw_edge_list$citing_id,
                           'to' = raw_edge_list$cited_id)
   
   
-  attr_list <- data_frame('id' = raw_attr_list$article_id,
+  attr_list <- data.frame('id' = raw_attr_list$article_id,
                           'type' = raw_attr_list$node_type,
                           'year' = raw_attr_list$publication_year)
   
-  search_date <- data_frame('id' = raw_search_date$article_id,
+  search_date <- data.frame('id' = raw_search_date$article_id,
                             'search_year' = raw_search_date$search_year,
                             'short_name' = raw_search_date$short_name)
   
@@ -57,15 +61,15 @@ create_salt_graph <- function(raw_edge_list_file,
   raw_search_date <- read.csv(raw_search_date_file)
   
   # filter the raw data
-  edge_list <- data_frame('from' = raw_edge_list$citing_ID,
+  edge_list <- data.frame('from' = raw_edge_list$citing_ID,
                           'to' = raw_edge_list$cited_ID)
   
   
-  attr_list <- data_frame('id' = raw_attr_list$ID,
+  attr_list <- data.frame('id' = raw_attr_list$ID,
                           'type' = raw_attr_list$node_type,
                           'year' = raw_attr_list$year)
   
-  search_date <- data_frame('id' = raw_search_date$ID,
+  search_date <- data.frame('id' = raw_search_date$ID,
                             'search_year' = raw_search_date$last_search_year, 
                             'short_name' = raw_search_date$short_name)
   
@@ -85,56 +89,59 @@ create_salt_graph <- function(raw_edge_list_file,
   )
 }
 
-# return adjusted average jaccard similarity for an SRR in a specific year
-avg_adjusted_js <- function(year,
-                            srr_name,
-                            G){
+#
+# compute adjusted jaccard similarity -- new function
+#
+# input: the two srrs' name
+# output: their adjusted Jaccard similarity
+adjusted_js <- function(srr_1_name, srr_2_name, G){
   
-  # create vids
-  vids <- which(V(G)$year <= year)
-  target_srr_id <- which(V(G)$name == srr_name)
+  # convert name to id
+  srr_1_id <- which(V(G)$name == srr_1_name)
+  srr_2_id <- which(V(G)$name == srr_2_name)
   
-  if (!(target_srr_id %in% vids)){
+  # find the search year of srr_1 and srr_2
+  srr_1_year <- V(G)[srr_1_id]$year
+  srr_2_year <- V(G)[srr_2_id]$year
+  
+  # chose smaller of srr_1_year and srr_2_year as the subgraphing year
+  subgraph_year <- min(srr_1_year, srr_2_year)
+  
+  # subgraph ids
+  vids <- which(V(G)$year <= subgraph_year)
+  
+  # add back srr_1_id or srr_2_id if either of them is not in the vids
+  if (!(srr_1_id %in% vids)){
     
-    vids <- c(vids, target_srr_id)
-              
+    vids <- c(vids, srr_1_id)
+    
   }
+  
+  if (!(srr_2_id %in% vids)){
+    
+    vids <- c(vids, srr_2_id)
+    
+  }  
+  
   
   # construct the subgraph
   G_sub <- igraph::induced.subgraph(graph=G, vids = vids)
   
-  # compute Jaccard Similarity
-  srr_vids <- which(V(G_sub)$type == "Systematic Review Report")
+  # recompute vids for srr_1 and srr_2
+  srr_1_id_sub <- which(V(G_sub)$name == srr_1_name)
+  srr_2_id_sub <- which(V(G_sub)$name == srr_2_name)
   
-  js_matrix <- igraph::similarity(
+  # compute Jaccard similarity of srr_1 and srr_2
+  
+  adjusted_js <- igraph::similarity(
+    
     graph = G_sub,
-    vids = srr_vids,
-    mode = 'out',
+    vids = c(srr_1_id_sub, srr_2_id_sub),
+    mode = "out",
     method = 'jaccard'
   )
   
-  # compute average by: melt the js_matrix, remove repeats and diagonals, compute average
-  js_df <- reshape::melt(js_matrix)
-  colnames(js_df) <- c("v1", "v2", "jaccard")
+  # select the diagnoal element to return
+  return(adjusted_js[1,2])
   
-  # convert ids to names
-  for (i in 1:nrow(js_df))
-  {
-    v1 <- js_df$v1[i]
-    v2 <- js_df$v2[i]
-    js_df$v1[i] <- as.integer(igraph::V(G_sub)[v1]$name)
-    js_df$v2[i] <- as.integer(igraph::V(G_sub)[v2]$name)
-    
   }
-  
-  # remove entries where v1 == v2
-  rows_to_keep = which((js_df$v1 < js_df$v2) & 
-                         (js_df$v1 == srr_name | js_df$v2 == srr_name))
-  
-  js_df = js_df[rows_to_keep,]
-  avg_adjusted_js <- mean(js_df$jaccard)
-  
-  return(list(year=year, 
-              srr_name = srr_name, 
-              avg_adjusted_js = avg_adjusted_js))
-}
