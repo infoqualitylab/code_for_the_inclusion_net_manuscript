c6f0b66719f971af609c8593c129adf658f11eae
yuanxiesa <40753490+yuanxiesa@users.noreply.github.com>
2022-12-20 14:46:41 -0600

    Update d_ratio.R

--- R/d_ratio.R
+++ R/d_ratio.R
@@ -1,6 +1,6 @@
 # d_ratio.R
 
-# This script produces Figure 2 and Figure 5 of the manuscript "Exploring Evidence Selection with the Inclusion Network"
+# This script produces Figure 5 of the manuscript "Exploring Evidence Selection with the Inclusion Network"
 # by Yuanxi Fu, Caitlin Vitosky Clarke, Mark Van Moer, and Jodi Schneider
 
 # author: Yuanxi Fu
@@ -65,42 +65,5 @@ median(d_ratio_df_salt$d_ratio) # 0
 
 sqrt(var(d_ratio_df_exrx$d_ratio)) # 0.3031006
 sqrt(var(d_ratio_df_salt$d_ratio)) # 0.1031187
-
-# Figure 2
-# regenerate d_ratio_df_exrx
-d_ratio_df_exrx <- compute_d_ratio(edge_list_exrx)
-
-d_ratio_df_exrx$name <- stringr::str_c("#", 
-                                       as.character(d_ratio_df_exrx$srr))
-
-isolated_srr <- c(3, 23, 26)
-periphery_srr <- c(1, 4, 5, 6, 11, 13, 14, 15, 17, 18, 19, 20, 24, 27, 28)
-core_srr <- c(2, 8, 9, 10, 12, 16, 21, 22, 25)
-
-d_ratio_df_exrx$type <- case_when(
-  
-  d_ratio_df_exrx$srr %in% isolated_srr ~ 'isolated',
-  d_ratio_df_exrx$srr %in% periphery_srr ~ 'periphery',
-  d_ratio_df_exrx$srr %in% core_srr ~ 'core'
-)
-
-d_ratio_df_exrx$type <- factor(d_ratio_df_exrx$type, levels = c('core', 'periphery', 'isolated'))
-
-# create a color-blind friendly palette
-cbPalette <- c("#E69F00", "#56B4E9", "#009E73")
-
-ggplot(data=d_ratio_df_exrx, aes(x=reorder(name, -d_ratio), y=d_ratio, fill=type, color=type)) +
-  geom_bar(stat = "identity") +
-  labs(y = "Dandelion-ness Ratio", x = 'SRR') +
-  scale_fill_manual(values=cbPalette) +
-  scale_colour_manual(values=cbPalette) +
-  theme_bw() +
-  theme(legend.title= element_blank(), 
-        legend.text = element_text(size = 16),
-        axis.text = element_text(size = 16),
-        axis.title.x = element_text(margin = margin(t = 8, r = 0, b = 0, l = 0, unit = 'pt'),
-                                    size = 18),
-        axis.title.y = element_text(margin = margin(t = 0, r = 8, b = 0, l = 0, unit = 'pt'),
-                                    size = 18))
 
 
